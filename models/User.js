363e63a43fa18e938ca8fecab23d3e810cb17c76
EladRobin <elad1999@hotmail.com>
2025-06-23 19:45:47 +0300

    aa

--- models/User.js
+++ models/User.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   fullName: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false } // שדה חדש
 });
 
 module.exports = mongoose.model('User', userSchema);
+
+
+
